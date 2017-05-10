7b3e4a54275f1750d33b8c7cb983214a30cda3da
jessicaprt <jessicaprieto31@gmail.com>
2017-05-09 22:52:05 -0600

    added scroll animation effect

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 	$("#name").fadeIn(2000);
+	console.log("hello");
+	
+	var y = $("#box").offset().top;
+	console.log($("#box").offset().top);
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > y-150) {
+			$(".inner-box-disp").fadeIn(1500);
+			$(".i-h1").fadeIn(1500);
+			$(".i-h2").fadeIn(1500);
+		}
+	});	
 });
