38b0360e04f0e2e8b3b34b706854854ce78f69ad
jessicaprt <jessicaprieto31@gmail.com>
2017-05-09 22:57:50 -0600

    scroll animation timing

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	console.log($("#box").offset().top);
 
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > y-150) {
+		if ($(this).scrollTop() > y*0.6) {
 			$(".inner-box-disp").fadeIn(1500);
 			$(".i-h1").fadeIn(1500);
 			$(".i-h2").fadeIn(1500);
